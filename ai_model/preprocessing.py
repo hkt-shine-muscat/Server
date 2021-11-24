aae5cbe860432e8b599a696f7f00b696bd96bc9f
nid2S <s20034@gsm.hs.kr>
2021-11-25 07:07:42 +0900

    don'n use kcs

--- ai_model/preprocessing.py
+++ ai_model/preprocessing.py
@@ -11,8 +11,8 @@ def make_dataset():
     print("Data Making Start.")
     # ChatbotData
     chatbot_data = pd.read_csv("data/raw_data/ChatbotData.csv", names=["dialogue", "response", "labels"]).loc[1:].drop(["labels"], axis=1)
-    chatbot_data["dialogue"].apply(lambda row: row+"</s>")
-    chatbot_data["response"].apply(lambda row: "<s>"+row+"</s>")
+    chatbot_data["dialogue"] = chatbot_data["dialogue"].apply(lambda row: row+"</s>")
+    chatbot_data["response"] = chatbot_data["response"].apply(lambda row: "<s>"+row+"</s>")
     trainData = trainData.append(chatbot_data)
     print("ChatbotData Complete.")
     # emotional
@@ -47,35 +47,37 @@ def make_dataset():
         except KeyError:
             continue
     print("KoreanConversation Complete.")
-    # kcs
-    for TorV in ["train", "valid"]:
-        print("KCS " + TorV + " start.")
-        for filename in os.listdir("./data/raw_data/kcs_"+TorV):
-            kcs_data = json.load(open("./data/raw_data/kcs_"+TorV+"/"+filename, "r+", encoding="utf-8"))["data"]
-            print(TorV+" "+filename+" start.")
-            for conv in kcs_data:
-                temp = ""
-                temp_sent = ""
-                pre_turn = ""
-                for dialogue in conv["body"]["dialogue"]:
-                    if dialogue["turnID"] != pre_turn:
-                        if dialogue["turnID"] == "T1":
-                            if TorV == "train":
-                                trainData = trainData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
-                            else:
-                                valData = valData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
-                            temp = ""
-                            temp_sent = ""
-                        elif int(dialogue["turnID"][1:]) % 2 == 0:
-                            temp += temp_sent + "</s>"
-                        else:
-                            if TorV == "train":
-                                trainData = trainData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
-                            else:
-                                valData = valData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
-                            temp += temp_sent + "</s>"
-                    temp_sent += dialogue["utterance"] + " "
-    print("KoreanConversationSummary Complete.")
+    # # kcs
+    # trainData = pd.DataFrame(columns=["dialogue", "response"])
+    # valData = pd.DataFrame(columns=["dialogue", "response"])
+    # for TorV in ["train", "valid"]:
+    #     print("KCS " + TorV + " start.")
+    #     for filename in os.listdir("./data/raw_data/kcs_"+TorV):
+    #         kcs_data = json.load(open("./data/raw_data/kcs_"+TorV+"/"+filename, "r+", encoding="utf-8"))["data"]
+    #         print(TorV+" "+filename+" start.")
+    #         for conv in kcs_data:
+    #             temp = ""
+    #             temp_sent = ""
+    #             pre_turn = ""
+    #             for dialogue in conv["body"]["dialogue"]:
+    #                 if dialogue["turnID"] != pre_turn:
+    #                     if dialogue["turnID"] == "T1":
+    #                         if TorV == "train":
+    #                             trainData = trainData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
+    #                         else:
+    #                             valData = valData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
+    #                         temp = ""
+    #                         temp_sent = ""
+    #                     elif int(dialogue["turnID"][1:]) % 2 == 0:
+    #                         temp += temp_sent + "</s>"
+    #                     else:
+    #                         if TorV == "train":
+    #                             trainData = trainData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
+    #                         else:
+    #                             valData = valData.append(pd.DataFrame([[temp, "<s>" + temp_sent + "</s>"]], columns=["dialogue", "response"]))
+    #                         temp += temp_sent + "</s>"
+    #                 temp_sent += dialogue["utterance"] + " "
+    # print("KoreanConversationSummary Complete.")
     # save
     trainData.dropna()
     valData.dropna()
@@ -124,5 +126,5 @@ class Preprocesser:
     def decoding(self, ids: list[int]):
         return self.tokenizer.batch_decode(ids, skip_special_tokens=True)
 
-
+make_dataset()
 

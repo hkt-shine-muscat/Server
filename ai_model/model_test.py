959ffda48ff4fd350b92cbd2e7511c7a38bd7baf
nid2S <s20034@gsm.hs.kr>
2021-12-09 09:27:24 +0900

    main setting, train setting

--- ai_model/model_test.py
+++ ai_model/model_test.py
@@ -2,19 +2,20 @@ from preprocessing import Preprocesser
 import tensorflow as tf
 import sys
 
-p = Preprocesser()
-model = tf.keras.models.load_model("./model/tf_model")
-try:
-    text = sys.argv[1]
-except IndexError:
-    text = input("User >> ")
+if __name__ == "__name__":
+    p = Preprocesser()
+    model = tf.keras.models.load_model("./model/tf_model")
+    try:
+        text = sys.argv[1]
+    except IndexError:
+        text = input("User >> ")
 
-output = model.generate(p.encoding(text),
-                        max_length=1000,
-                        num_beams=5,
-                        top_k=20,
-                        no_repeat_ngram_size=4,
-                        length_penalty=0.65,
-                        repetition_penalty=2.
-                        )
-print(p.decoding(output))
+    output = model.generate(p.encoding(text),
+                            max_length=1000,
+                            num_beams=5,
+                            top_k=20,
+                            no_repeat_ngram_size=4,
+                            length_penalty=0.65,
+                            repetition_penalty=2.
+                            )
+    print(p.decoding(output))
--- ai_model/model_traning.py
+++ ai_model/model_traning.py
@@ -1,14 +1,28 @@
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
 from transformers import TFAutoModelForCausalLM
 from preprocessing import Preprocesser
 import tensorflow as tf
 
-p = Preprocesser()
-model = TFAutoModelForCausalLM.from_pretrained(p.PREMODEL_NAME, from_pt=True)
+def lr_scheduler(epoch, lr):
+    if epoch < 2:
+        return 5e-5
+    elif epoch < 4:
+        return 3e-5
+    else:
+        return 1e-5
 
-optim = tf.keras.optimizers.Adam(learning_rate=p.lr)
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model.compile(loss=loss, optimizer=optim, metrics="accuracy")
-model.fit(p.getTrainData(), validation_data=p.getValidationData(), batch_size=p.batch_size, epochs=4,
-          callbacks=[EarlyStopping(patience=3), ModelCheckpoint("./model/tf_model", monitor="val_accuracy", save_best_only=True)])
-model.save("./model/last_model.h5")
+
+if __name__ == "__main__":
+    p = Preprocesser()
+    model = TFAutoModelForCausalLM.from_pretrained(p.PREMODEL_NAME, from_pt=True)
+    history = ""
+    # loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    for optim in ["adam", "rmsprop", "nadam"]:
+        model.compile(loss=loss, optimizer=optim, metrics="accuracy")
+        hist = model.fit(p.getTrainData(), validation_data=p.getValidationData(), batch_size=p.batch_size, epochs=5,
+                         callbacks=[EarlyStopping(patience=3), LearningRateScheduler(lr_scheduler),
+                                    ModelCheckpoint("./model/"+optim+"_model", monitor="val_accuracy", save_best_only=True)])
+        model.save("./model/"+optim+"_last_model.h5")
+        history += optim + " : " + str(hist) + "\n\n"
+    open("./data/history.txt", "w+", encoding="utf-8").write(history)
